9a9931fc4ce5ddcd6020d6553c78f62d7653c6b0
vj9032 <vj9032@gmail.com>
2025-09-22 22:23:06 +0530

    stable clean-code still ugly

--- js/login.js
+++ js/login.js
@@ -1,48 +1,78 @@
 let taskName = document.querySelector("#tsk-name")
 let taskDesc = document.querySelector("#tsk-desc")
 let submit = document.getElementById("submit")
-submit.onclick = sbmit
-let tasks = []
 let main = document.getElementById("itemList")
 let taskBox = document.createElement("div")
 taskBox.setAttribute("id" , "task")
+submit.onclick = sbmit
+
+
+
+function check(){
+    if (taskName.value== "" || taskDesc.value== "" ){
+        return false
+    }
+    return true
+}
+
 function sbmit(){
-    let priority = document.querySelector('input[name="priority"]:checked')
-    let taskBox = document.createElement("div")
-    taskBox.setAttribute("id" , "task")
-    let doneBtn = document.createElement("button")
-    let deleteBtn = document.createElement("button")
-    doneBtn.setAttribute("class" , "btns")
-    deleteBtn.setAttribute("class" , "btns")
-    doneBtn.innerText = "✅"
-    deleteBtn.innerText = "❌"
-    deleteBtn.onclick = function() {
-        remove(deleteBtn)
+    if(check()){
+        let priority = document.querySelector('input[name="priority"]:checked')
+        let taskBox = document.createElement("div")
+        let doneBtn = document.createElement("button")
+        let deleteBtn = document.createElement("button")
+        let btnz = document.createElement("div")
+
+        taskBox.setAttribute("id" , "task")
+        doneBtn.setAttribute("class" , "btns")
+        deleteBtn.setAttribute("class" , "btns")
+        
+        deleteBtn.onclick = function() {
+            remove(deleteBtn)
+        }
+        doneBtn.onclick = function(){
+            completed(doneBtn)
+        }
+        let tsknm = document.createElement("li")
+        let tskdec = document.createElement("li")
+        let tskPriority = document.createElement('li')
+        let unLis = document.createElement("ul")
+        unLis.appendChild(tsknm)
+        unLis.appendChild(tskdec)
+        unLis.appendChild(tskPriority)
+
+        doneBtn.innerText = "✅"
+        deleteBtn.innerText = "❌"
+        tskPriority.innerText = priority.value
+        tsknm.innerHTML = `<h1>${taskName.value}</h1>`
+        tskdec.innerText = taskDesc.value
+
+        btnz.appendChild(doneBtn)
+        btnz.appendChild(deleteBtn)
+        
+        taskBox.appendChild(unLis)
+        taskBox.appendChild(btnz)
+
+
+        main.appendChild(taskBox)
+
+        //reset inputs
+        taskName.value = null
+        taskDesc.value = null
+        priority.checked= false
+
     }
-    doneBtn.onclick = function(){
-        completed(doneBtn)
+    else{
+        alert("enter valid task and task description")
     }
-    let tsknm = document.createElement("h2")
-    let tskdec = document.createElement("p")
-    let tskPriority = document.createElement('h3')
-    tskPriority.innerText = priority.value
-    tsknm.innerText = taskName.value
-    tskdec.innerText = taskDesc.value
-    taskBox.appendChild(doneBtn)
-    taskBox.appendChild(deleteBtn)
-    taskBox.appendChild(tsknm)
-    taskBox.appendChild(tskdec)
-    taskBox.appendChild(tskPriority)
-    main.appendChild(taskBox)
+    
 }
 
 function remove(button){
-    button.parentElement.remove();
+    button.parentElement.parentElement.remove();
 }
 
 function completed(button){
-    button.parentElement.style.backgroundColor = "green"
+    button.parentElement.parentElement.style.backgroundColor = "green"
     // button.parentElement.style.textDecoration = "line-through"
 }
-
-console.log(tasks)
